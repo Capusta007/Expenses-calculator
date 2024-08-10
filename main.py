74b8a9caaf63ebf98cf64cc53d975fcfe5949749
Capusta007 <dimacharnenko@gmail.com>
2024-08-11 01:26:34 +0300

    Added report screen

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ class MainScreen(Screen):
     def go_to_addition_screen(self):
         self.manager.current = "addition_screen"
 
+    def go_to_report_screen(self):
+        self.manager.current = "report_screen"
+
 
 class AdditionScreen(Screen):
+    def go_to_main_screen(self):
+        self.manager.current = "main_screen"
+
+
+class ReportScreen(Screen):
     def go_to_main_screen(self):
         self.manager.current = "main_screen"
 
@@ -20,6 +28,7 @@ class MyApp(App):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name="main_screen"))
         sm.add_widget(AdditionScreen(name="addition_screen"))
+        sm.add_widget(ReportScreen(name="report_screen"))
         return sm
 
 
